ea12550475d0fd4336185aa62010d21809867a92
cathalgreaney <c.greaney@lse.ac.uk>
2020-04-14 17:28:38 +0100

    minor app.js change just to force a travis rebuild

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Sample app created using containers.
+          Sample app created using containers-checking aws.
         </p>
         <a
           className="App-link"
